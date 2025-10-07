8eb54ba8aa17aeddd40fd934bb51b2fad48a52b1
Anand Vishwakarma <rd.anandv@gmail.com>
2025-10-07 21:36:50 +0530

    made pdf questions extraction part dynamic

--- main.py
+++ main.py
@@ -13,22 +13,23 @@ def home():
 @app.route("/upload-pdf", methods=["POST"])
 def upload_pdf():
     try:
-        file = request.files['pdf']  
+        file_data = request.files['pdf']  
+         # 2. get "setting" metadata (JSON string)
+        setting_raw = request.form.get("setting")
         # debug log length
         # print("Received PDF data of length:", (file))
         # check pdf exists
-        if not file:
+        if not file_data:
             return jsonify({"error": "No PDF received"}), 400
-         # 2. get "setting" metadata (JSON string)
-        setting_raw = request.form.get("setting")
         if not setting_raw:
             return jsonify({"error": "No setting provided"}), 400
         setting = json.loads(setting_raw)
         # print("Received setting:", setting)
         # 3. save file to disk (or process in memory)
-        save_path = os.path.join("uploads", file.filename)
+        save_path = os.path.join("uploads", file_data.filename)
+        # print('save path', save_path)
         os.makedirs("uploads", exist_ok=True)
-        file.save(save_path)
+        file_data.save(save_path)
         # Process the PDF with settings
         result = handle_uploaded_pdf(save_path, setting)
         # print("Processing result:", result)
@@ -37,8 +38,16 @@ def upload_pdf():
         # success response
         return jsonify({"message": "PDF processed successfully", "result": result or {}}), 200
     except Exception as e:
+        print("exception error", e)
         # error response
         return jsonify({"error": str(e)}), 500
+    finally:
+        if save_path and os.path.exists(save_path):
+            try:
+                os.remove(save_path)
+            except Exception as cleanup_error:
+                # Optionally log this error
+                print(f"Failed to delete temporary file: {cleanup_error}")
 # run app
 if __name__ == "__main__":
     app.run(debug=True)
